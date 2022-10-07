94a4fe0b6f740182cd0f96a98a958a251c14433e
Joshua Tenorio <joshliam09@gmail.com>
2022-10-06 23:49:57 -0700

    worked on num spiral

--- cses/num_spiral.c
+++ cses/num_spiral.c
@@ -7,5 +7,15 @@ int main(){
     for(int i = 0; i < t; i++){
         int y, x; // row, column
         scanf("%d %d", &y, &x);
+        if(x == 1 && y == 1) printf("1\n");
+        else if(x < y){ // column < row
+            printf("%d\n", y*y - (x-1));
+        }
+        else if(x > y){ // column > row
+            printf("%d\n", x*x - (y-1));
+        }
+        else if(x == y){ // row = column
+            printf("%d\n", x*x - x+1);
+        }
     }
 }
