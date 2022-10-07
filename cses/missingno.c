23fe96b0a4803145a1314e5ef7df182d34d70a30
Joshua Tenorio <joshliam09@gmail.com>
2022-10-06 22:01:34 -0700

    finish missingno

--- cses/missingno.c
+++ cses/missingno.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+int getSumTo(int n);
+
 int main(){
     int n;
-    scanf("%d\n", &n);
+    scanf("%d", &n);
+    int sum = getSumTo(n);
+    int num;
+    for(int i = 0; i < n-1; i++){
+        scanf("%d", &num);
+        sum -= num;
+    }
+    printf("%d", sum);
+}
 
-    // TODO scan line 2 of input
+int getSumTo(int n){
+    int sum = 0;
+    for(int i = 1; i <=n; i++) sum += i;
+
+    return sum;
 }
