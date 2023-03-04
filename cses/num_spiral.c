9b0126ca08595e62e89bcaf9c9f7ce837d61e5bf
Joshua Tenorio <joshliam09@gmail.com>
2023-03-03 19:48:15 -0700

    number spiral ree

--- cses/num_spiral.c
+++ cses/num_spiral.c
@@ -1,21 +1,46 @@
 #include <stdio.h>
 
+int abs(int x) {
+    if (x >= 0) return x;
+    else return x * -1;
+}
 
+int getSection(int col) {
+    if (col % 2 == 0) {
+        return 0;
+    }
+    else return col*col;
+}
 int main(){
     int t;
     scanf("%d", &t);
-    for(int i = 0; i < t; i++){
+    for (int i = 0; i < t; i++){
         int y, x; // row, column
         scanf("%d %d", &y, &x);
-        if(x == 1 && y == 1) printf("1\n");
-        else if(x < y){ // column < row
-            printf("%d\n", y*y - (x-1));
-        }
-        else if(x > y){ // column > row
-            printf("%d\n", x*x - (y-1));
-        }
-        else if(x == y){ // row = column
+
+
+        if (x == y){ // row = column
             printf("%d\n", x*x - x+1);
+        }
+        else if (x > y) {
+            if (x % 2 == 1) {
+                int base = getSection(x);
+                printf("%d\n", base - y + 1);
+            }
+            else {
+                int base = getSection(x-1) + 1;
+                printf("%d\n", base + y -1);
+            }
+        }
+        else { 
+            if (y % 2 == 1) {
+                int base = getSection(y);
+                printf("%d\n", base + x - 1);
+            }
+            else {
+                int base = getSection(y-1) + 1;
+                printf("%d\n", base - x +1);
+            }
         }
     }
 }
